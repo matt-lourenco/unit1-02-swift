1fde38e97853eb03d9529d2f2b0b9e26b35a2c0e
Matt <matthew.lourenco@ocsbstudent.ca>
2018-02-06 14:48:15 -0500

    Update maxLogsCalculator.swift

--- maxLogsCalculator.swift
+++ maxLogsCalculator.swift
@@ -6,14 +6,19 @@
 //  Created by Matthew Lourenco on 05/02/18.
 //  Copyright © 2018 MTHS. All rights reserved.
 //
-
 var maxWeightCapacity: Double = 1100
 var weightPerMetre: Double = 20
 var maxTotalLength: Double = maxWeightCapacity / weightPerMetre
 
-print("Enter the length of the logs: ")
-var input = readLine(strippingNewline: true)
-print(input!)
-var lengthOfLog = Double(input!)
-var maxNumberOfLogs = maxTotalLength / Double(input!)!
-print("A maximum of \(maxNumberOfLogs) logs can fit on the truck.")
+while true {
+	print("Enter the length of the logs: ")
+    var input = readLine(strippingNewline: true)
+	if var check = Double(input!), check != nil {
+		var lengthOfLog = Double(input!)
+		var maxNumberOfLogs = maxTotalLength / Double(input!)!
+		print("A maximum of \(maxNumberOfLogs) logs can fit on the truck.")
+		break
+	} else {
+		print("Please enter a number")
+	}
+}
